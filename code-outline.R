dd8a92e48c78479c40994e821cbe00a85baec515
François Michonneau <francois.michonneau@gmail.com>
2018-06-01 12:30:11 +0100

    remove mention of 60+ dataset in code

--- code-outline.R
+++ code-outline.R
@@ -7,7 +7,6 @@ library(gapminder)
 
 gm_car_deaths <- read_tsv("data/gapminder-car-deaths.txt")
 gm_cells      <- read_tsv("data/gapminder-cell-phones.txt")
-gm_60plus     <- read_tsv("data/gapminder_population_60plus.csv")
 
 ## Let's put all of this data in a single tidy dataset
 
@@ -19,11 +18,6 @@ gm_car_deaths_tidy <- gm_car_deaths %>%
 gm_cells_tidy <- gm_cells %>%
     rename(country = cell_phones) %>%
     gather(year, cell_subscribers, -country)
-
-
-gm_60plus_tidy <- gm_60plus %>%
-    rename(country = population_60plus) %>%
-    gather(year, pop_60plus, -country)
 
 ## Use left-join to put the data together
 
@@ -40,16 +34,11 @@ gm_cells_tidy <- gm_cells_tidy %>%
     mutate(year = as.integer(year),
            cell_subscribers = as.integer(cell_subscribers))
 
-gm_60plus_tidy <- gm_60plus_tidy %>%
-    mutate(year = as.integer(year),
-           pop_60plus = as.integer(pop_60plus))
-
 ## Let's try the join again
 
 gm <- gapminder %>%
     left_join(gm_car_deaths_tidy, by = c("country", "year")) %>%
-    left_join(gm_cells_tidy, by = c("country", "year")) %>%
-    left_join(gm_60plus_tidy, by = c("country", "year"))
+    left_join(gm_cells_tidy, by = c("country", "year"))
 
 ## demonstrate transmute and mutate on cell_subscribers
 
